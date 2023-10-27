eed3ba79fbc4ded668aed3566c6b6c947829354e
María José M <95242045+Marrye24@users.noreply.github.com>
2023-10-27 17:06:01 -0600

    Fase II, III, IV y V

--- CRISP.py
+++ CRISP.py
@@ -158,3 +158,138 @@ sorted_correlations = target_correlations.abs().sort_values(ascending=False)
 filtered_df = filtered_df[['Inflight entertainment', 'Ease of Online booking', 'Online support', 'On-board service', 'Seat comfort', 'Online boarding', 'Leg room service', 'Customer Type', 'Class', 'Baggage handling', 'Checkin service', 'Cleanliness', 'Inflight wifi service', 'target']]
 
 #filtered_df.to_csv('file.csv', index=False)  
+
+#%% FASE IV. MODELADO DE DATOS
+#%%% Train & Test split
+
+df = pd.read_csv("file.csv")
+
+X = df.drop("target", axis=1)  # Características
+y = df["target"]  # Variable objetivo
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+#%%% Regresión Logística
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Predecir las etiquetas en el conjunto de prueba
+y_pred = model.predict(X_test)
+
+# Calcular métricas de evaluación
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+
+
+#%%% Random Forest
+
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)  # Puedes ajustar los hiperparámetros según sea necesario
+rf_model.fit(X_train, y_train)
+
+# Predecir las etiquetas en el conjunto de prueba
+y_pred_rf = rf_model.predict(X_test)
+
+# Calcular métricas de evaluación
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+precision_rf = precision_score(y_test, y_pred_rf)
+recall_rf = recall_score(y_test, y_pred_rf)
+f1_rf = f1_score(y_test, y_pred_rf)
+conf_matrix_rf = confusion_matrix(y_test, y_pred_rf)
+roc_auc_rf = roc_auc_score(y_test, y_pred_rf)
+
+
+#%%% Naive Bayes
+
+nb_model = MultinomialNB()
+nb_model.fit(X_train, y_train)
+
+# Predecir las etiquetas en el conjunto de prueba
+y_pred_nb = nb_model.predict(X_test)
+
+# Calcular métricas de evaluación
+accuracy_nb = accuracy_score(y_test, y_pred_nb)
+precision_nb = precision_score(y_test, y_pred_nb)
+recall_nb = recall_score(y_test, y_pred_nb)
+f1_nb = f1_score(y_test, y_pred_nb)
+conf_matrix_nb = confusion_matrix(y_test, y_pred_nb)
+
+
+#%% FASE V. EVALUACIÓN
+
+#%%% Métricas de evaluación
+#     Regresión logística
+print('\nLogistic Regression\n')
+print(f'Accuracy: {accuracy}')
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+print(f'F1-Score: {f1}')
+print(f'Confusion Matrix:\n{conf_matrix}')
+print(f'ROC AUC: {roc_auc}')
+
+
+#       Bosque Aleatorio
+print('\nRandom Forest\n')
+print(f' Accuracy: {accuracy_rf}')
+print(f'Precision: {precision_rf}')
+print(f'Recall: {recall_rf}')
+print(f'F1-Score: {f1_rf}')
+print(f'Confusion Matrix:\n{conf_matrix_rf}')
+print(f'ROC AUC: {roc_auc_rf}')
+
+
+#       Naive Bayes
+print('\nNaive Bayes\n')
+print(f'Accuracy: {accuracy_nb}')
+print(f'Precision: {precision_nb}')
+print(f'Recall: {recall_nb}')
+print(f'F1-Score: {f1_nb}')
+print(f'Confusion Matrix:\n{conf_matrix_nb}')
+
+
+#%%% Gráficas ROC
+
+#     Regresión logística
+y_prob = model.predict_proba(X_test)[:,1]  # Probabilidad de la clase positiva
+fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, label=f'AUC = {roc_auc:.2f}')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Logistic Regression - Receiver Operating Characteristic (ROC) Curve')
+plt.legend()
+plt.show()
+
+#       Bosque Aleatorio
+y_prob_rf = rf_model.predict_proba(X_test)[:,1]  # Probabilidad de la clase positiva
+fpr_rf, tpr_rf, thresholds_rf = roc_curve(y_test, y_prob_rf)
+
+plt.figure(figsize=(8, 6))
+plt.plot(fpr_rf, tpr_rf, label=f'AUC = {roc_auc_rf:.2f}')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Random Forest - Receiver Operating Characteristic (ROC) Curve')
+plt.legend()
+plt.show()
+
+
+#       Naive Bayes
+y_prob_nb = nb_model.predict_proba(X_test)[:, 1]  # Probabilidad de la clase positiva
+fpr_nb, tpr_nb, thresholds_nb = roc_curve(y_test, y_prob_nb)
+roc_auc_nb = roc_auc_score(y_test, y_prob_nb)
+
+plt.figure(figsize=(8, 6))
+plt.plot(fpr_nb, tpr_nb, label=f'AUC = {roc_auc_nb:.2f}')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Multinomial Naive Bayes - Receiver Operating Characteristic (ROC) Curve')
+plt.legend()
+plt.show()
